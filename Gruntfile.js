ab80bfda38e0b1b3c9b7f67b6dbee7ed6f35a1cf
Richard Bailey <additive.inverse@gmail.com>
2023-01-18 21:39:11 -0800

    updating and moving some things around

--- Gruntfile.js
+++ Gruntfile.js
@@ -120,7 +120,7 @@ module.exports = function(grunt) {
 				updateConfigs: [],
 				commit: true,
 				commitMessage: 'Release v%VERSION%',
-				commitFiles: ['package.json', 'bower.json'],
+				commitFiles: ['package.json', 'bowe`r.json'],
 				createTag: true,
 				tagName: '%VERSION%',
 				tagMessage: '%VERSION%',
